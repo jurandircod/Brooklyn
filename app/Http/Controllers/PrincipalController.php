3821df462ff0f91581e8dcfca7ad3e049105fbc6
jurandircod <jurandiraparecido19651965@gmail.com>
2025-12-06 16:29:00 -0300

    teste

--- app/Http/Controllers/PrincipalController.php
+++ app/Http/Controllers/PrincipalController.php
@@ -25,7 +25,6 @@ class PrincipalController extends Controller
         
         // Carrega todos os produtos COM suas fotos (eager loading)
         $produtos = Produto::with('fotos')->where('estado', 'ativo')->take(12)->get();
-        dd($produtos);
         $produtoDestaque = $produtos->first();
         $categorias = Categoria::whereIn('nome', ['camisas', 'skates', 'tenis'])->get();
         $itens = $itens ?? [];
